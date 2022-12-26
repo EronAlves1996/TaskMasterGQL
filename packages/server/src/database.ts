2bd293cbcc4c3840a7668b3cd740a9c7c1afab3e
Eron Alves <eron.alves@rocketmail.com>
2022-12-26 16:08:35 -0300

    chore: include better log messages

--- packages/server/src/database.ts
+++ packages/server/src/database.ts
@@ -3,6 +3,7 @@ import config from "./config";
 
 async function connectToDatabase() {
   await mongoose.connect(config.MONGO_URL);
+  console.log("Connected to database");
 }
 
 export default connectToDatabase;
--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -6,4 +6,4 @@ const server = createServer(app.callback());
 connectToDatabase();
 
 server.listen(3000);
-console.log("Listening on port 3000")
+console.log("Listening on port 3000");
