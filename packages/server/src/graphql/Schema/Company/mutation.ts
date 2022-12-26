4c552be2d24ae4925e313738ad23f36bf9a7a822
Eron Alves <eron.alves@rocketmail.com>
2022-12-26 09:12:39 -0300

    refactor: create only fields for query and mutation on Company

--- packages/server/src/graphql/Schema/Company/mutation.ts
+++ packages/server/src/graphql/Schema/Company/mutation.ts
@@ -1,29 +1,26 @@
-import { GraphQLObjectType, GraphQLString } from "graphql";
+import { GraphQLFieldConfig, GraphQLString, ThunkObjMap } from "graphql";
 import { createCompany, updateCompany } from "./resolvers";
 import companyTypes from "./type";
 
 const type = companyTypes.objectType;
 const company = { type: companyTypes.inputType };
 
-const mutation = new GraphQLObjectType({
-  name: "Mutation",
-  fields: {
-    createCompany: {
-      type,
-      args: {
-        company,
-      },
-      resolve: createCompany,
+const mutation: ThunkObjMap<GraphQLFieldConfig<any, any, any>> = {
+  createCompany: {
+    type,
+    args: {
+      company,
     },
-    updateCompany: {
-      type,
-      args: {
-        _id: { type: GraphQLString },
-        company,
-      },
-      resolve: updateCompany,
-    },
+    resolve: createCompany,
   },
-});
+  updateCompany: {
+    type,
+    args: {
+      _id: { type: GraphQLString },
+      company,
+    },
+    resolve: updateCompany,
+  },
+};
 
 export default mutation;
--- packages/server/src/graphql/Schema/Company/query.ts
+++ packages/server/src/graphql/Schema/Company/query.ts
@@ -1,22 +1,24 @@
-import { GraphQLList, GraphQLObjectType, GraphQLString } from "graphql";
+import {
+  GraphQLFieldConfig,
+  GraphQLList,
+  GraphQLString,
+  ThunkObjMap,
+} from "graphql";
 import { getCompanies, getCompany } from "./resolvers";
 import companyTypes from "./type";
 
-const query = new GraphQLObjectType({
-  name: "Query",
-  fields: {
-    getCompany: {
-      type: companyTypes.objectType,
-      args: {
-        _id: { type: GraphQLString },
-      },
-      resolve: getCompany,
+const query: ThunkObjMap<GraphQLFieldConfig<any, any, any>> = {
+  getCompany: {
+    type: companyTypes.objectType,
+    args: {
+      _id: { type: GraphQLString },
     },
-    getCompanies: {
-      type: new GraphQLList(companyTypes.objectType),
-      resolve: getCompanies,
-    },
+    resolve: getCompany,
   },
-});
+  getCompanies: {
+    type: new GraphQLList(companyTypes.objectType),
+    resolve: getCompanies,
+  },
+};
 
 export default query;
