88734d503fb26987d8eb8f1456718137e576e8fc
Eron Alves <eron.alves@rocketmail.com>
2022-12-26 16:12:38 -0300

    refactor: adapt resolvers to correct version of graphql

--- packages/server/src/graphql/Schema/Company/resolvers.ts
+++ packages/server/src/graphql/Schema/Company/resolvers.ts
@@ -1,28 +1,18 @@
 import { GraphQLFieldResolver } from "graphql";
 import db from "./db";
 
-export const getCompany: GraphQLFieldResolver<any, any, any, unknown> = async (
+export const getCompany: GraphQLFieldResolver<any, any> = async (_, { _id }) =>
+  await db.findById(_id);
+
+export const getCompanies: GraphQLFieldResolver<any, any> = async () =>
+  await db.findAll();
+
+export const createCompany: GraphQLFieldResolver<any, any> = async (
   _,
-  { _id }
-) => await db.findById(_id);
+  { company }
+) => await db.create(company);
 
-export const getCompanies: GraphQLFieldResolver<
-  any,
-  any,
-  any,
-  unknown
-> = async () => await db.findAll();
-
-export const createCompany: GraphQLFieldResolver<
-  any,
-  any,
-  any,
-  unknown
-> = async (_, { company }) => await db.create(company);
-
-export const updateCompany: GraphQLFieldResolver<
-  any,
-  any,
-  any,
-  unknown
-> = async (_, { _id, company }) => await db.update(_id, company);
+export const updateCompany: GraphQLFieldResolver<any, any> = async (
+  _,
+  { _id, company }
+) => await db.update(_id, company);
--- packages/server/src/graphql/Schema/User/resolvers.ts
+++ packages/server/src/graphql/Schema/User/resolvers.ts
@@ -1,32 +1,13 @@
 import { GraphQLFieldResolver } from "graphql";
 import db from "./db";
 
-export const createUser: GraphQLFieldResolver<any, any, any, unknown> = async (
-  _,
-  { user }
-) => {
+export const createUser: GraphQLFieldResolver<any, any> = async (_, { user }) =>
   await db.create(user);
-};
-
-export const updateUser: GraphQLFieldResolver<any, any, any, unknown> = async (
+export const updateUser: GraphQLFieldResolver<any, any> = async (
   _,
   { _id, user }
-) => {
-  await db.update(_id, user);
-};
-
-export const getUser: GraphQLFieldResolver<any, any, any, unknown> = async (
-  _,
-  { _id }
-) => {
+) => await db.update(_id, user);
+export const getUser: GraphQLFieldResolver<any, any> = async (_, { _id }) =>
   await db.findById(_id);
-};
-
-export const getUsers: GraphQLFieldResolver<
-  any,
-  any,
-  any,
-  unknown
-> = async () => {
+export const getUsers: GraphQLFieldResolver<any, any> = async () =>
   await db.findAll();
-};
