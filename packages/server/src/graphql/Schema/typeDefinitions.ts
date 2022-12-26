d93a79329dd39924152ba83f9501a05429462304
Eron Alves <eron.alves@rocketmail.com>
2022-12-26 16:09:15 -0300

    feat: declare type definitions

---
+++ packages/server/src/graphql/Schema/typeDefinitions.ts
@@ -0,0 +1,34 @@
+import { GraphQLInputObjectType, GraphQLObjectType } from "graphql";
+
+export interface graphqlTypes {
+  objectType: GraphQLObjectType;
+  inputType: GraphQLInputObjectType;
+}
+
+export interface Task {
+  _id?: string;
+  description: string;
+  isCompleted: boolean;
+  projectId: string;
+}
+
+export interface Company {
+  _id?: string;
+  name: string;
+}
+
+export interface Project {
+  _id?: string;
+  name: string;
+  description: string;
+  company: Company;
+  deadline: Date;
+  tasks: Task[];
+}
+
+export interface User {
+  _id?: string;
+  name: string;
+  company: Company;
+  projects: Project[];
+}
