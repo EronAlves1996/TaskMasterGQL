cfbbfbd05585af5d84e6c7ce32f7401933f37560
Eron Alves <eron.alves@rocketmail.com>
2022-12-27 13:59:32 -0300

    build: change port configuration

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -5,5 +5,5 @@ import connectToDatabase from "./database";
 const server = createServer(app.callback());
 connectToDatabase();
 
-server.listen(3000);
-console.log("Listening on port 3000");
+server.listen(3001);
+console.log("Listening on port 3001");
