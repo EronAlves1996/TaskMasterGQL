406097362694037c86f240bb77a5d85d5e7e84d9
Eron Alves <eron.alves@rocketmail.com>
2022-12-27 23:34:07 -0300

    fix: adjust endpoints for proxy

--- packages/web/src/config.ts
+++ packages/web/src/config.ts
@@ -1,6 +1,4 @@
 export const config = {
-  LOGIN_ENDPOINT:
-    process.env.REACT_APP_LOGIN_ENDPOINT || "http://localhost:3001/login",
-  GRAPHQL_ENDPOINT:
-    process.env.REACT_APP_GRAPHQL_ENDPOINT || "http://localhost:3001/graphql",
+  LOGIN_ENDPOINT: process.env.REACT_APP_LOGIN_ENDPOINT || "/login",
+  GRAPHQL_ENDPOINT: process.env.REACT_APP_GRAPHQL_ENDPOINT || "/graphql",
 };
