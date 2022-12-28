8ba19cf9d6d830680d1743bc3c29e9e776cdd7c7
Eron Alves <eron.alves@rocketmail.com>
2022-12-28 01:39:29 -0300

    feat: create cache for user information

--- packages/web/src/pages/App/App.tsx
+++ packages/web/src/pages/App/App.tsx
@@ -1,10 +1,25 @@
-import { Outlet } from 'react-router-dom';
-import './App.css';
+import { Outlet } from "react-router-dom";
+import "./App.css";
+import { useState } from "react";
+
+interface User {
+  _id?: string;
+  name: string;
+  email: string;
+  password: string;
+  company: string;
+  projects: string[];
+}
+
+export type CtxType = [
+  User | null,
+  React.Dispatch<React.SetStateAction<User | null>>
+];
 
 function App() {
-  return (
-    <Outlet />
-  );
+  const [user, setUser] = useState<User | null>(null);
+
+  return <Outlet context={[user, setUser]} />;
 }
 
 export default App;
