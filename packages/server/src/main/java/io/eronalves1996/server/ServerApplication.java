f3a10c3e9850bd92a69db578d16b96c405c996a6
Eron Alves <eron.alves@rocketmail.com>
2022-12-29 00:44:25 -0300

    chore: scaffold spring maven project for server

---
+++ packages/server/src/main/java/io/eronalves1996/server/ServerApplication.java
@@ -0,0 +1,13 @@
+package io.eronalves1996.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerApplication.class, args);
+	}
+
+}
